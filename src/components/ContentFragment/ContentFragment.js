ad721debcc9a91702b7ee4bb7a1a5f7043a37726
David Feldbaum <david.feldbaum@icfolson.com>
2019-07-09 15:05:02 -0500

    Added API fetch to content fragment component

--- src/components/ContentFragment/ContentFragment.js
+++ src/components/ContentFragment/ContentFragment.js
@@ -2,22 +2,48 @@ import React, { Component } from 'react';
 
 class ContentFragment extends Component {
 
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+
+		this.state = {
+      items: [],
+      isLoaded: false,
+		}
+	}
+
+  componentDidMount() {
+    fetch('http://localhost:4502/bin/fragmentexamples/movies.json')
+      .then(res => res.json())  //res is result from api, convert to json format
+      .then(json => {  //take json and set json data to state.items
+        this.setState({
+          isLoaded: true,
+          items: json,
+        })
+      });
+    }
 
   render() {
-
     console.log(this.props);
 
-    return (
+    var { isLoaded, items } = this.state  //access items from state in render
 
-      <div className="content-fragment">
-        <p>First Name: David</p>
-        <p>Last Name: Feldbaum</p>
-        <p>Company: ICF Next</p>
-      </div>
-    );
+    if (!isLoaded) {
+      return <div>Loading...</div>;
+    }
+
+    else {
+      return (
+        <div className="content-fragment">
+          <ul>
+            {items.map(item => ( //loop each obj from api result 
+              <li key={item.modelTitle}>
+                {item.title}
+              </li> 
+            ))}; 
+          </ul> 
+        </div>
+      )
+    }
   }
 }
 
